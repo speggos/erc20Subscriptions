e2e564d6c14ad2681f161ad5ddbfbe962d742833
speggos <spencercox@hotmail.com>
2022-11-11 16:16:37 -0800

    - Create Subscription tests complete

--- contracts/Erc20Subscription.sol
+++ contracts/Erc20Subscription.sol
@@ -8,34 +8,31 @@ contract Erc20Subscription {
     uint32 public numberOfSubscriptions;
     // TODO pack this
     struct Subscription {
-        address from;
-        address to;
+        address payer;
+        address payee;
         address token;
         uint32 id;
-        uint total;
-        uint paid;
-        uint start;
-        uint end;
+        uint64 frequency;
+        uint amount;
+        uint nextPayment;
     }
     Subscription[] public subscriptions;
 
-
-    event Payment(uint32 indexed subscriptionId, address indexed from, address indexed to, uint amount, uint token);
-    event PaymentFailed(uint32 indexed subscriptionId, address indexed from, address indexed to, uint amount, uint token);
-    event NewSubscription(uint32 indexed subscriptionId, address indexed from, address indexed to);
-    event SubscriptionFinished(uint32 indexed subscriptionId, address indexed from, address indexed to);
-    event SubscriptionCancelled(uint32 indexed subscriptionId, address indexed from, address indexed to);
+    event Payment(uint32 indexed subscriptionId, address indexed payer, address indexed payee, uint amount, address token);
+    event PaymentFailed(uint32 indexed subscriptionId, address indexed payer, address indexed payee, uint amount, address token);
+    event NewSubscription(uint32 indexed subscriptionId, address indexed payer, address indexed payee);
+    event SubscriptionFinished(uint32 indexed subscriptionId, address indexed payer, address indexed payee);
+    event SubscriptionCancelled(uint32 indexed subscriptionId, address indexed payer, address indexed payee);
 
     //TODO validate transfer functions exist on erc20. What are security risks?
-    // TODO Allow start to be anything? Pending transactions
-    function createSubscription(address _to, address _token, uint _total, uint _end) public {
+    // TODO payments collected only monthly/yearly? Who cares?
+    function createSubscription(address _payee, address _token, uint64 _frequency, uint _amount) public {
         numberOfSubscriptions += 1;
-        subscriptions.push(Subscription(msg.sender, _to, _token, numberOfSubscriptions, _total, 0, block.timestamp, _end));
-        ERC20(_token).approve(address(this), _total);
-    }
+        bool success = ERC20(_token).transferFrom(msg.sender, _payee, _amount);
+        require(success, "Not enough allowance");
 
-    function getRemaining(uint32 id) public view returns(uint) {
-        Subscription memory sub = subscriptions[id-1];
-        return sub.total - sub.paid;
+        subscriptions.push(Subscription(msg.sender, _payee, _token, numberOfSubscriptions, _frequency, _amount, block.timestamp + _frequency));
+        emit NewSubscription(numberOfSubscriptions, msg.sender, _payee);
+        emit Payment(numberOfSubscriptions, msg.sender, _payee, _amount, _token);
     }
 }
