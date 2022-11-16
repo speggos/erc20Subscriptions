80c59b6ab6424b2579b4024682ebbf9c194fc865
speggos <spencercox@hotmail.com>
2022-11-15 20:12:54 -0800

    - hardhat config changed for deploy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,9 +3,18 @@ import "@nomicfoundation/hardhat-toolbox";
 import '@typechain/hardhat'
 import '@nomiclabs/hardhat-ethers'
 import "@nomicfoundation/hardhat-chai-matchers"
+import * as dotenv from 'dotenv';
+dotenv.config()
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    gelri: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      // @ts-ignore
+      accounts: [process.env.WALLET_PRIVATE_KEY]
+    }
+  }
 };
 
 export default config;
